43679ce393358dccc803f31ca2c7945b9b8eb887
LaCodon <webmaster@lacodon.de>
2015-10-05 21:44:27 +0200

    Try to fix exception issue
    PHPUnit needs full namespace path

--- unittests/RegistryTest.php
+++ unittests/RegistryTest.php
@@ -1,17 +1,12 @@
 <?php
 
 use Famework\Registry\Famework_Registry;
-use Famework\Registry\Famework_Exception_Registry_Error;
-use Famework\Db\Famework_Exception_Database_Fail;
 
 class RegistryTest extends PHPUnit_Framework_TestCase {
 
     public function setUp() {
         Famework_Registry::set('foo', 'bar');
         Famework_Registry::setEnv('Test');
-        // call errors once to autoload them, maybe PHPUnit bug
-        (new Famework_Exception_Registry_Error(NULL, NULL));
-        (new Famework_Exception_Database_Fail(NULL, NULL));
     }
 
     public function testGet() {
@@ -21,14 +16,14 @@ class RegistryTest extends PHPUnit_Framework_TestCase {
     }
 
     /**
-     * @expectedException Famework_Exception_Registry_Error
+     * @expectedException Famework\Registry\Famework_Exception_Registry_Error
      */
     public function testGetEmpty() {
         Famework_Registry::get('empty', TRUE);
     }
 
     /**
-     * @expectedException Famework_Exception_Database_Fail
+     * @expectedException Famework\Db\Famework_Exception_Database_Fail
      */
     public function testGetDb() {
         Famework_Registry::getDb();
