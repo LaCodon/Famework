abcde573155b42403179482ad387d2ff418883ee
LaCodon <webmaster@lacodon.de>
2017-01-12 14:23:43 +0100

    Update Famework_Handler.php

--- Handler/Famework_Handler.php
+++ Handler/Famework_Handler.php
@@ -39,6 +39,7 @@ class Famework_Handler {
             echo '<tr><td>Exception</td><td>&nbsp;&nbsp;&nbsp;&nbsp;Message</td></tr>';
             printf('<tr><td>%s:%d</td><td>&nbsp;&nbsp;&nbsp;&nbsp;%s</td></tr>', $exception->getFile(), $exception->getLine(), $exception->getMessage());
             echo '</table>';
+            die('here');
             header('Internal Server Error', TRUE, 500);
             die();
         }
